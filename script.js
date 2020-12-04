a858b0600ffa035fd2bc985c27e44c2a00926f81
Richard <richardjalba@gmail.com>
2020-12-04 13:54:51 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const cards = document.querySelectorAll('.cards');
+
+cards.forEach((card) => {
+  card.addEventListener('click', () => {
+    removeActiveClass();
+    card.classList.add('active');
+  });
+});
+
+function removeActiveClass() {
+  cards.forEach((card) => {
+    card.classList.remove('active');
+  });
+}
